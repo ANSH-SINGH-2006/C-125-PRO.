6147e45fb7c2efe52a6d3867206edb84ab186633
Ansh Singh <64472289+ANSH-SINGH-2006@users.noreply.github.com>
2021-12-21 17:48:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from PIL import Image
+import cv2
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+import os, ssl, time
 import PIL.ImageOps
 
 
-X,y = fetch_openml('mnist_784', version=1, return_X_y= True)
-X_train, X_test, y_train, y_test= train_test_split(X,y, random_state=9,train_size=7500, test_size=2500)
+X= np.load("image.npz") ['arr=0']
+y=pd.read_csv("labels.csv") ["labels"]
+classes= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+nclasses= len(classes)
+X_train, X_test, y_train, y_test= train_test_split(X,y, random_state=9,train_size=3500, test_size=500)
 
 X_train_scaled= X_train/255.0
 X_test_scaled= X_test/255.0
